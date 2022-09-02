b26f24eaa6b886581c1195409d052a7b187e4d3a
kiyounkim <21900072@handong.ac.kr>
2022-09-02 21:10:16 +0900

    WordManager commit

---
+++ src/main/java/WordManager.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class WordManager {
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+    public int selectMenu() {
+        System.out.print("***영단어 마스터***\n"
+                + "******************\n"
+                + "1. 모든 단어 보기\n"
+                + "2. 수준별 단어 보기\n"
+                + "3. 단어 검색\n"
+                + "4. 단어 추가\n"
+                + "5. 단어 수정\n"
+                + "6. 단어 삭제\n"
+                + "7. 파일 저장\n"
+                + "0. 나가기\n"
+                + "******************\n"
+                + "=> 원하는 메뉴는? ");
+        return s.nextInt();
+    }
+    public void start() {
+        while(true) {
+            int menu = selectMenu();
+            System.out.println(menu);
+            if(menu==0) {
+                System.out.println("사전을 종료합니다.");
+                break;
+            }
+            if(menu==1) {
+                //list_all
+                wordCRUD.listAll();
+            }
+            else if(menu==2) {
+                //list_grade
+            }
+            else if(menu==3) {
+                //search
+            }
+            else if(menu==4) {
+                //create
+                wordCRUD.addWord();
+            }
+            else if(menu==5) {
+                //update
+            }
+            else if(menu==6) {
+                //delete
+            }
+            else if(menu==7) {
+                //Save
+            }
+        }
+    }
+}
