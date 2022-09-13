27668e383df195d7bec432831fcd4e0c82e8d0da
kiyounkim <21900072@handong.ac.kr>
2022-09-13 10:05:20 +0900

    Probably final project

--- src/main/java/WordManager.java
+++ src/main/java/WordManager.java
@@ -22,7 +22,7 @@ public class WordManager {
         return s.nextInt();
     }
     public void start() {
-        //wordCRUD.loadFile();
+        wordCRUD.loadFile();
         while(true) {
             int menu = selectMenu();
             //System.out.println(menu);
